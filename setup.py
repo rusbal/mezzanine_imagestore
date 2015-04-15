23a8c9eebe5d74d2f8fda4ff95fbf3bed2643b3c
Raymond Usbal <raymond@philippinedev.com>
2015-04-15 18:12:01 +0800

    prefix mezzanine

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = 'imagestore',
+        name = 'mezzanine_imagestore',
         version = '2.7.7',
         packages = find_packages(),
         install_requires = [
